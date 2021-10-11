02aab196bd45a85f0b4d11e20bcacf8b457f370a
TheRealOkone <tnoko@yandex.ru>
2021-10-11 17:35:42 +0300

    zip added

--- src/main/java/com/pack/Main.java
+++ src/main/java/com/pack/Main.java
@@ -1,7 +1,61 @@
 package com.pack;
 
+import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
+import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+import org.apache.commons.compress.archivers.sevenz.SevenZOutputFile;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+
+import java.io.*;
+import java.util.zip.*;
+
 public class Main {
     public static void main(String[] args) {
+        String filename = "C:\\SomeDir\\notes.txt";
+        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("C:\\SomeDir\\output.zip"));
+            FileInputStream fis= new FileInputStream(filename);)
+        {
+            ZipEntry entry1=new ZipEntry("notes.txt");
+            zout.putNextEntry(entry1);
+            // считываем содержимое файла в массив byte
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            // добавляем содержимое к архиву
+            zout.write(buffer);
+            // закрываем текущую запись для новой записи
+            zout.closeEntry();
+        }
+        catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+
+        try(ZipInputStream zin = new ZipInputStream(new FileInputStream("C:\\SomeDir\\output.zip")))
+        {
+            ZipEntry entry;
+            String name;
+            long size;
+            while((entry=zin.getNextEntry())!=null){
+
+                name = entry.getName(); // получим название файла
+                size=entry.getSize();  // получим его размер в байтах
+                System.out.printf("File name: %s \t File size: %d \n", name, size);
+
+                // распаковка
+                FileOutputStream fout = new FileOutputStream("C:\\somedir\\new" + name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        }
+        catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+        }
+
 
     }
 }
